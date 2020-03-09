82d094dcb40cbcc9235438e3ffcc7a27a920cea7
unknown <arek91w@gmail.com>
2020-03-09 10:06:12 +0100

    main convert function working

--- converse.py
+++ converse.py
@@ -28,18 +28,34 @@ print("Liczba dziesietnie wynosi: " + str(number_decimal)) """
  
 code_table = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '9': 8, 'A': 10, 'a': 10, 'B': 11, 'b': 11, 'C': 12, 'c': 12, 'D': 13, 'd': 13, 'E': 14, 'e': 14, 'F': 15, 'f': 15}
 code_table_reverse = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'}
+
 def convert(number, basisIn, basisOut):
     value = 0
     number = str(number)
+    # calculating  the value in the decimal system
     for i in range(1, len(number)+1):
         value += code_table[(number)[-i]]*basisIn**(i-1)
-    print(value)
+    
+    # calculating length of the number saved to the new system
     length = 0
     while basisOut**length < value:
         length += 1
-    print(length)
-    for i in range(1, basisOut):
-        print(i)
-convert(92, 16, 4)
-
-print(code_table['e'])
+    
+    output = ''
+    
+    
+    for i in range(length - 1, -1, -1):
+        asigned = False
+        for j in range(0, basisOut+1):
+            if asigned == True:
+                continue
+            if basisOut**i*j > value:
+                if j == 0:
+                    output += code_table_reverse[j]
+                    asigned = True
+                else:
+                    output += code_table_reverse[j-1]
+                    value -= basisOut**i*(j-1)
+                    asigned = True
+    return output
+print(convert('a15', 16, 10))
