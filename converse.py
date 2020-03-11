efda010a02c9edaf6378e6ce4911a23d9c266075
unknown <arek91w@gmail.com>
2020-03-11 15:32:50 +0100

    short description of the project added

--- converse.py
+++ converse.py
@@ -1,3 +1,6 @@
+# The project serves a function converting any number from  a 2-16-base number record system to another 2-16-base system
+# First parameter takes a number, second a basis in (number from the range 2-16) and this basis out (2-16)
+
 code_table = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,  'A': 10, 'a': 10, 'B': 11, 'b': 11, 'C': 12, 'c': 12, 'D': 13, 'd': 13, 'E': 14, 'e': 14, 'F': 15, 'f': 15}
 code_table_reverse = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'}
 
